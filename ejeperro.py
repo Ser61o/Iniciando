190bfcc6a301a46d5c4e36c3e6c0994641d7ac93
Ser61o <sergiofvmg@gmail.com>
2024-09-13 04:26:00 +0000

    Pending changes exported from your codespace

---
+++ ejeperro.py
@@ -0,0 +1,17 @@
+'''import perro
+
+perro.ladrar()'''
+
+'''from lib import perro
+perro.ladrar()'''
+
+
+from lib.perro import ladrar
+ladrar()
+
+import math
+print(math.sqrt(9))
+raiz=math.sqrt(4)
+print(raiz)
+
+
